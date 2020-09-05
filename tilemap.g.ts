0c1d66caef83d7c806c78acc491409d53b635c57
jimj2010 <jimj2010@gmail.com>
2020-09-05 16:30:35 -0700

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,31 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`1000100001010101010101010101010101010101040202020202020202020201020202010101010201010101010102010201020101020102010202020202020102010201010201020102010101010101020102010102010201020202020202020201020101020202010101010101010101010201010201020202020202020102020202010102010101010102020201020202020101020102020201020202010202020201010201020102010101010101010101010102010201020102020202020202020101020102010201020101010101010201010202020102010201020202020102010102020102020202020202020201020201010101010101010101010101010103`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . 2 . . . 2 
+2 2 2 . 2 2 2 2 2 2 . 2 . 2 . 2 
+2 . 2 . 2 . . . . . . 2 . 2 . 2 
+2 . 2 . 2 . 2 2 2 2 2 2 . 2 . 2 
+2 . 2 . 2 . . . . . . . . 2 . 2 
+2 . . . 2 2 2 2 2 2 2 2 2 2 . 2 
+2 . 2 . . . . . . . 2 . . . . 2 
+2 . 2 2 2 2 2 . . . 2 . . . . 2 
+2 . 2 . . . 2 . . . 2 . . . . 2 
+2 . 2 . 2 . 2 2 2 2 2 2 2 2 2 2 
+2 . 2 . 2 . 2 . . . . . . . . 2 
+2 . 2 . 2 . 2 . 2 2 2 2 2 2 . 2 
+2 . . . 2 . 2 . 2 . . . . 2 . 2 
+2 . . 2 . . . . . . . . . 2 . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+`, [myTiles.transparency16,sprites.dungeon.collectibleInsignia,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.chestClosed,sprites.dungeon.stairWest], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
